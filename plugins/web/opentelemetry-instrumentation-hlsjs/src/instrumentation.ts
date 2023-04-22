4bf786de91dd75989f5cc3062e64212645346bd7
negtak <50316517+negtak@users.noreply.github.com>
2023-04-22 19:16:07 +0900

    tmp

--- plugins/web/opentelemetry-instrumentation-hlsjs/src/instrumentation.ts
+++ plugins/web/opentelemetry-instrumentation-hlsjs/src/instrumentation.ts
@@ -12,16 +12,31 @@ export class HlsJsInstrumentation extends InstrumentationBase {
 
   private _patchAttachMedia() {
     const tracer = this.tracer;
-    return function patchAttachMedia(this: Hls, original: Function) {
-      const span = tracer.startSpan('hlsjs.attachMedia');
-      const result = original.apply(this, arguments);
-      console.log("===test===");
-      span.end();
-      return result;
-    }
+    return (original: any) => {
+      return function patchAttachMedia(this: Hls, ...args: any[]) {
+        const span = tracer.startSpan('hlsjs.attachMedia');
+        const result = original.apply(this, args);
+        span.end();
+        return result;
+      };
+    };
   }
+
+  private _patchStopLoad() {
+    const tracer = this.tracer;
+    return (original: any) => {
+      return function patchStopLoad(this: Hls, ...args: any[]) {
+        const span = tracer.startSpan('hlsjs.stopLoad');
+        const result = original.apply(this, args);
+        span.end();
+        return result;
+      };
+    };
+  }
+
   override enable() {
     this._wrap(Hls.prototype, 'attachMedia', this._patchAttachMedia());
+    this._wrap(Hls.prototype, 'stopLoad', this._patchStopLoad());
   }
 
   override disable() {
